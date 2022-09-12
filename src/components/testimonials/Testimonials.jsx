c74c713e1eb5ba9b014dd9d5aa91fbd2ce89122b
Claudbest1 <claudiusluv50@gmail.com>
2022-09-12 03:34:29 +0100

    update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR1 from '../../assets/Bethan Smith.jpg'
 import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
@@ -19,8 +19,8 @@ import 'swiper/css/pagination';
 const data = [
   {
     avatar: AVTR1,
-    name: 'Daniel Makinde',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quos veritatis porro, tempore consectetur in impedit ullam suscipit et voluptate explicabo laudantium, dolorum odit non fuga! Ad, molestias! Est, hic?'
+    name: 'Bethan Smith',
+    review: "I worked with Claudius during a Charity Project, whereby I hired him to create marketing content for our fundraising campaign. All involved in our campaign were absolutely blown away by the marketing which Claudius created. His creative ideas, ability to draw the readers attention to key details, and overall style is absolutely second to none. I also found Claudius to be incredibly polite, prompt and thorough. Would absolute recommend and hope to work with Claudius again very soon."
   },
   {
     avatar: AVTR2,
